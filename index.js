aee1322f421bf1430c95dbfc89007ed6da2c9fee
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2024-12-05 04:30:15 -0300

    Add configuration files and improve SVG assets
    - Introduced .editorconfig for consistent coding styles across the project.
    - Updated .gitignore to exclude .DS_Store files.
    - Added biome.jsonc for configuration settings related to the Biome formatter and linter.
    - Enhanced JavaScript code in index.js with type annotations and improved event handling for the mobile menu.
    - Created jsconfig.json for JavaScript project configuration.
    - Refined SVG assets for better formatting and structure.
    - Added VSCode settings and extensions recommendations for improved development experience.

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const hamburger = document.querySelector('.hamburger');
-const mobileMenu = document.querySelector('.mobile-menu');
-const overlay = document.querySelector('.overlay');
+/** @type {HTMLDivElement | null} */
+const hamburger = document.querySelector('.hamburger')
+/** @type {HTMLDivElement | null} */
+const mobileMenu = document.querySelector('.mobile-menu')
+/** @type {HTMLDivElement | null} */
+const overlay = document.querySelector('.overlay')
+/** @type {NodeListOf<HTMLAnchorElement>} */
+const navLinks = document.querySelectorAll('.link')
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    mobileMenu.classList.toggle('active');
-    overlay.classList.toggle('active');
-});
+if (hamburger && mobileMenu && overlay && !(navLinks.length < 0)) {
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active')
+    mobileMenu.classList.toggle('active')
+    overlay.classList.toggle('active')
+  })
 
-// cierra el menu cuando clickeas :v
-const navLinks = document.querySelectorAll('.link');
-navLinks.forEach(link => {
+  navLinks.forEach(link => {
     link.addEventListener('click', () => {
-        hamburger.classList.remove('active');
-        mobileMenu.classList.remove('active');
-        overlay.classList.remove('active');
-    });
-});
+      hamburger.classList.remove('active')
+      mobileMenu.classList.remove('active')
+      overlay.classList.remove('active')
+    })
+  })
 
-overlay.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    mobileMenu.classList.remove('active');
-    overlay.classList.remove('active');
-});
+  overlay.addEventListener('click', () => {
+    hamburger.classList.remove('active')
+    mobileMenu.classList.remove('active')
+    overlay.classList.remove('active')
+  })
+}
